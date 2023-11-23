ce119b7da1d80a1e3d81e8130d9e2f724ea3e8e6
MasterBowtie <greenducky12@hotmail.com>
2023-11-23 12:25:38 -0700

    reorganized project

---
+++ src/pause_menu.py
@@ -0,0 +1,37 @@
+import pygame
+
+def menu(screen, mouse_pos):
+    pygame.mouse.set_visible(True)
+    menu_base = pygame.image.load("../images/menu_base.png")
+    menu_button = pygame.image.load("../images/menu_button.png")
+    menu_pos = (screen.get_size()[0]/2 - menu_base.get_size()[0]/2, screen.get_size()[1]/2 - menu_base.get_size()[1]/2)
+    contiue_pos = (screen.get_size()[0]/2 - menu_button.get_size()[0]/2, menu_pos[1]* 4/2)
+    exit_pos = (screen.get_size()[0]/2 - menu_button.get_size()[0]/2, menu_pos[1]* 6/2)
+    running = True
+    while running:
+        for event in pygame.event.get(pygame.KEYDOWN):
+            if event.dict["key"] == 27:
+                running = False
+
+        screen.blit(menu_base, menu_pos)
+        screen.blit(menu_button, exit_pos)
+
+
+        for event in pygame.event.get(pygame.MOUSEBUTTONDOWN):
+            left = exit_pos[0]
+            top = exit_pos[1]
+            right = left + menu_button.get_size()[0]
+            bottom = top + menu_button.get_size()[1]
+            pos = event.dict["pos"]
+            if left < pos[0] < right and top < pos[1] < bottom:
+                exit()
+
+
+
+        pygame.display.update()
+        for event in pygame.event.get(pygame.QUIT):
+            if event.type == pygame.QUIT:
+                exit()
+
+    pygame.mouse.set_pos(mouse_pos)
+    pygame.mouse.set_visible(False)
---
+++ src/ship.py
@@ -0,0 +1,76 @@
+import pygame
+from src.gun import Gun
+import math
+
+class Ship:
+    def __init__(self, ship, position, rotation=0):
+        self.__ship_image = ship
+        self.__position = position
+        self.__rotation = rotation
+        self.__gun_front = None
+        self.__gun_back = None
+        self.__moves = [False, False, False, False]
+
+    def drawShip(self, screen):
+        temp = pygame.transform.rotate(self.__ship_image, self.__rotation)
+        center = (temp.get_size()[0]/2, temp.get_size()[1]/2)
+        screen.blit(temp, (self.__position[0] - center[0], self.__position[1] - center[1]))
+        if self.__gun_front is not None and self.__gun_back is not None:
+            self.__gun_front.drawGun(screen)
+            self.__gun_back.drawGun(screen)
+
+
+    def hitBox(self):
+        temp = pygame.transform.rotate(self.__ship_image, self.__rotation)
+        center = (temp.get_size()[0] / 2, temp.get_size()[1] / 2)
+        top = self.__position[0] - center[0]
+        right = self.__position[1] + center[1]
+        bottom = self.__position[0] + center[0]
+        left = self.__position[1] - center[1]
+        return (top, right, bottom, left)
+
+    def getPos(self):
+        return self.__position
+
+    def getRotation(self):
+        return self.__rotation
+
+    def moveToggle(self, moves):
+        self.__moves = moves
+
+    def getMoving(self):
+        return self.__moves
+
+    def turnShip(self, angle, speed):
+        self.__rotation += angle
+        if self.__rotation < 0:
+            self.__rotation += 360
+        if self.__rotation > 360:
+            self.__rotation -= 360
+
+        if self.__gun_front is not None and self.__gun_back is not None:
+            x = math.cos(self.__rotation * (2 * math.pi /360)) * self.__ship_image.get_size()[0]/3
+            y = math.sin(self.__rotation * (2 * math.pi /360)) * self.__ship_image.get_size()[0]/3
+            self.__gun_front.setPosition((self.__position[0] + x, self.__position[1] - y))
+            self.__gun_back.setPosition((self.__position[0] - x, self.__position[1] + y))
+
+        self.moveShip(speed)
+
+
+    def moveShip(self, speed, axis=None):
+        x = math.cos(self.__rotation * (2 * math.pi / 360)) * speed
+        y = - math.sin(self.__rotation * (2 * math.pi / 360)) * speed
+
+        self.__position = (self.__position[0] + x, self.__position[1] + y)
+        if self.__gun_front is not None and self.__gun_back is not None:
+            self.__gun_front.setPosition((self.__gun_front.getPosition()[0] + x, self.__gun_front.getPosition()[1] + y))
+            self.__gun_back.setPosition((self.__gun_back.getPosition()[0] + x, self.__gun_back.getPosition()[1] + y))
+
+    def addFrontGun(self, gun_image):
+        self.__gun_front = Gun(gun_image,
+                         (self.__position[0] + (self.__ship_image.get_size()[0]/3),
+                          self.__position[1]))
+        self.__gun_back = Gun(gun_image,
+                               (self.__position[0] - (self.__ship_image.get_size()[0] / 3),
+                                self.__position[1]))
+
